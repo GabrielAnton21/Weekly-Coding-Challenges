2a300500c5e9cad787ab3dd099847fc0daa1326f
GabrielAnton21 <44251263+GabrielAnton21@users.noreply.github.com>
2022-05-12 12:56:41 -0400

    Add files via upload

---
+++ Weekly-Coding-Challenges/recursiveMultiply.java
@@ -0,0 +1,27 @@
+
+
+public class recursiveMultiply {
+   
+
+    static int recursivemultiply(int opA, int opB){
+            if(opB == 0 | opA == 0){
+                return 0;
+            }
+            else if(opB < 0){
+                return -(opA + recursivemultiply(opA, opB+1));
+            }
+            else {
+                return opA + recursivemultiply(opA, opB-1);
+            }
+            
+        
+        
+
+    }
+    public static void main(String[] args){
+
+        System.out.println(recursivemultiply(4, 5));
+    }
+}
+
+// BSC pw: RgReXNpbdZaD3
